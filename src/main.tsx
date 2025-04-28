9776576e54b86fd3f70036a6dd3b710ea83a9406
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-04-28 16:06:20 -0300

    Feature: inicializa o app criando os arquivos para as telas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import LoginScreen from './pages/LoginScreen';
+import HomeScreen from './pages/HomeScreen';
+
+
+const router = createBrowserRouter(
+  [
+    { path: '/', element: < LoginScreen />, },
+    { path: '/Home', element: < HomeScreen />, },
+  ]
+) 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/LoginScreen.tsx
@@ -0,0 +1,9 @@
+export default function LoginScreen() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+

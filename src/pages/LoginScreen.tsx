55584cbdf828c5f19f0cb0e1f04f3adace7d2541
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-04-28 16:56:26 -0300

    Feature: inicializa a divisao da pagina login

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,7 +1,29 @@
+import "../index.css"
+
 export default function LoginScreen() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div id="container">
+
+        <div className="Header">
+
+
+        </div>
+
+
+        {/*  Block for the login body contained by a div to organize the divs in blocks*/}
+        <div style={{width: "100%", height:"100%", display:'flex', flexDirection: 'row', gap: "3%"}}>
+            <div className="login_image">
+              <h1>Image</h1>
+            </div>
+
+            <div className="login_card">
+                <h1>Test</h1>
+            </div>
+        </div>
+
+        
+      </div>
     </>
   )
 }

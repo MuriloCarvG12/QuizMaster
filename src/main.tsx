fd92a1e5a33b16514bb17d11f609ef1e36248f52
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-04-28 16:12:45 -0300

    Feature: adiciona caminhos para todas as paginas

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,23 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import LoginScreen from './pages/LoginScreen';
 import HomeScreen from './pages/HomeScreen';
+import UserScreen from './pages/UserScreen';
+import CommunityScreen from './pages/CommunityScreen';
+import QuestionScreen from './pages/QuestionScreen';
+import ExamScreen from './pages/ExamScreen';
+import QuizScreen from './pages/QuizScreen';
 
 
 const router = createBrowserRouter(
   [
     { path: '/', element: < LoginScreen />, },
     { path: '/Home', element: < HomeScreen />, },
+    { path: '/UserScreen', element: < UserScreen />, },
+    { path: '/CommunityScreen', element: < CommunityScreen />, },
+    { path: '/QuestionScreen', element: < QuestionScreen />, },
+    { path: '/ExamScreen', element: < ExamScreen />, },
+    { path: '/QuizScreen', element: < QuizScreen />, },
+
   ]
 ) 
 

5b035a9477d630f4f370196ba4ced48665a27d10
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-04-29 13:52:59 -0300

    Feature: adiciona classes para visuais de campos de texto

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -14,22 +14,33 @@ export default function LoginScreen() {
         {/*  Block for the login body contained by a div to organize the divs in blocks*/}
         <div style={{width: "100%", height:"100%", display:'flex', flexDirection: 'row', gap: "3%", justifyContent: 'center'}}>
 
-            <div style={{display:'flex', flexDirection: 'column', width: "50%", marginLeft: "5%"}}>
+            <div style={{display:'flex', flexDirection: 'column', width: "50%", marginLeft: "5%", gap:"3%"}}>
 
                 <div className="login_image">
                     <h1>Image</h1>
                 </div>
+
                 <h1 style={{textAlign: 'center'}}>Frase Aleatoria Teste</h1>
 
             </div>
 
             <div style={{display:'flex', flexDirection: 'column', width: "50%"}}>
                 <div className="login_card_head">
-
+                    <h1 style={{textAlign: 'center'}}> Realizar Login!</h1>
                 </div>
 
                 <div className="login_card_body">
-                    
+
+                <div style={{width: "100%", height: "5%", display: "flex", justifyContent: "center", marginTop: "10%",marginBottom:"10%", alignSelf: "center", justifyItems:"center"}}>
+                        <input className="Text_Field" placeholder="Insira o seu nome"></input>
+                    </div>
+
+                    <div style={{width: "100%", height: "5%", display: "flex", justifyContent: "center", marginTop: "10%",marginBottom:"10%", alignSelf: "center", justifyItems:"center"}}>
+                        <input className="Text_Field" placeholder="Insira a sua senha"></input>
+                    </div>
+
+
+
                 </div>
 
               </div>

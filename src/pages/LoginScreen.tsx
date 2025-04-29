eaff376f27122aca17b9fefedf0f8c6ce7d4772a
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-04-29 13:24:29 -0300

    feature: adiciona a base para a tela login com a inserção de novas divs para organização do conteudo

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -12,15 +12,29 @@ export default function LoginScreen() {
 
 
         {/*  Block for the login body contained by a div to organize the divs in blocks*/}
-        <div style={{width: "100%", height:"100%", display:'flex', flexDirection: 'row', gap: "3%"}}>
-            <div className="login_image">
-              <h1>Image</h1>
+        <div style={{width: "100%", height:"100%", display:'flex', flexDirection: 'row', gap: "3%", justifyContent: 'center'}}>
+
+            <div style={{display:'flex', flexDirection: 'column', width: "50%", marginLeft: "5%"}}>
+
+                <div className="login_image">
+                    <h1>Image</h1>
+                </div>
+                <h1 style={{textAlign: 'center'}}>Frase Aleatoria Teste</h1>
+
             </div>
 
-            <div className="login_card">
-                <h1>Test</h1>
-            </div>
+            <div style={{display:'flex', flexDirection: 'column', width: "50%"}}>
+                <div className="login_card_head">
+
+                </div>
+
+                <div className="login_card_body">
+                    
+                </div>
+
+              </div>
         </div>
+           
 
         
       </div>

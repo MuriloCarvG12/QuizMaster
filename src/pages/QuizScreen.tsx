31ab73740ba1c7f2ac2c64f1832afb3405f116a7
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-05-05 14:34:09 -0300

    Feature: adiciona o botao para a tela quiz

--- src/pages/QuizScreen.tsx
+++ src/pages/QuizScreen.tsx
@@ -1,3 +1,4 @@
+import App_Button from "../components/App_Button"
 import Header from "../components/Header_navigation"
 
 
@@ -9,9 +10,28 @@ export default function QuizScreen() {
             <Header Username={""}/>
 
             <div style={{display:"flex", flexDirection:"column", width:"100%", height:"50%", justifyItems:"center", alignItems: "center"}}>
-                <div id="USER QUIZES HEAD" style={{width:"80%", height: "10%", backgroundColor:"#FDE4CF", textAlign:"center", color:"#666666"}}><h1>Meus Quizes</h1></div>
-                <div id="USER QUIZES" style={{display:"flex", flexDirection: "row", gap: "5%", width:"80%", height: "50%", backgroundColor:"#FDFDFD"}}></div>
+
+
+                <div id="USER QUIZES HEAD" 
+                style={{width:"80%", height: "9%", backgroundColor:"#FDE4CF", textAlign:"center", color:"#666666", paddingTop: "1%", borderStyle:"solid",borderWidth:"5px", borderColor:"#EBD5C3"}
+                }>
+                    <h1>Meus Quizes</h1>
+                </div>
+
+
+                <div id="USER QUIZES" 
+                style={{display:"flex", flexDirection: "row", gap: "5%", width:"80%", height: "50%", backgroundColor:"#FDFDFD", borderStyle:"solid",borderWidth:"5px",borderTopWidth:"0px", borderColor:"#E2E2E2"}
+                }>
+
+                </div>
+
+
             </div>
+
+            <div style={{width:"100%", height:"5%",display:"flex", justifyContent:"center"}}>
+                <App_Button bgcolor={"A8E6CF"} bordercolor={"99D2BD"} link={""} borderhovercolor={"7EAF9D"} bghovercolor={"99D2BD"} message={"CRIAR NOVO QUIZ"}/>
+            </div>
+            
             
 
             

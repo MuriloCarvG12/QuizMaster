67c62fceafcab2819922a8503d25d8055f3b9e80
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-05-06 18:11:40 -0300

    Adiciona o inicio das funcionalidades da tela de criação de simulados

--- src/pages/ExamScreen.tsx
+++ src/pages/ExamScreen.tsx
@@ -1,12 +1,42 @@
 import Header from "../components/Header_navigation";
 import App_Button from "../components/App_Button";
+import { useState } from "react";
 
 
 export default function ExamScreen() {
-    function teste(Frase:string)
+
+    const [current_step, set_current_step] = useState(0)
+    const [length, set_length] = useState(0)
+    
+
+    function Select_Length(NPerguntas:number)
     {
-        console.log(`${Frase}`)
+        set_length(NPerguntas)
+        set_current_step(current_step + 1)
+        console.log(length)
     }
+
+    // this funciton lets our program know which status of the exam generation the user is in
+    function RenderStatus(status: number) {
+        switch (status) {
+          case 0:
+            return <>
+                <App_Button bgcolor={"fce0d9"} bordercolor={"f2bcb1"}  borderhovercolor={"d1a097"} bghovercolor={"dec3bd"} onClick={() => Select_Length(25)} message={"Curto 25 perguntas"}/>
+                <App_Button bgcolor={"D4EDDA"} bordercolor={"ACCAB3"}  borderhovercolor={""} bghovercolor={"b3c7b8"} onClick={() => Select_Length(45)} message={"Médio 45 perguntas"}/>
+                <App_Button bgcolor={"D1ECF1"} bordercolor={"B7D8DE"}  borderhovercolor={""} bghovercolor={"b0cacf"} onClick={() => Select_Length(90)} message={"Longo 90 perguntas"}/>
+                <App_Button bgcolor={"FFF3CD"} bordercolor={"D5C799"}  borderhovercolor={""} bghovercolor={"ccc2a3"} onClick={() => Select_Length(1)} message={"Customizado"}/>;
+            </>
+          case 1:
+            return <>
+                <h1>Topicos</h1>
+            </>;
+          case 2:
+            return <p>Success!</p>;
+          default:
+            return <p>Unknown status</p>;
+        }
+      }
+
 
     return (
     
@@ -22,15 +52,14 @@ export default function ExamScreen() {
         <div id="HOME-USER-OPTIONS-HEAD" 
             style={{width: "60%", height: "5%", backgroundColor: "#B4FFFB", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#82D0D5"}}>
 
-            <h1 style={{color:"#333333", textAlign: "center", alignContent: "center"}}> Area do Usuario</h1>
+            <h1 style={{color:"#333333", textAlign: "center", alignContent: "center"}}> {current_step == 0 ? "Selecione o Tamanho da Prova" : current_step == 1 ? "Selecione os Topicos da Prova" : current_step == 2 ? "Selecione Adicionais da Prova" : "Isso nao deveria ter ocorrido"}</h1>
         </div>
-
+       
         <div id="HOME-USER-OPTIONS" className="User-Options" style={{width:"60%",height:"65%", borderStyle:"solid", borderWidth:"5px", borderColor:"#82D0D5", borderTop: "0px", justifyContent: "space-between", flexDirection:"column" }}>
-            <App_Button bgcolor={"fce0d9"} bordercolor={"f2bcb1"}  borderhovercolor={"d1a097"} bghovercolor={"dec3bd"} onClick={() => teste("curto")} message={"Curto"}/>
-            <App_Button bgcolor={"D4EDDA"} bordercolor={"ACCAB3"}  borderhovercolor={""} bghovercolor={"b3c7b8"} onClick={() => teste("medio")} message={"Médio"}/>
-            <App_Button bgcolor={"D1ECF1"} bordercolor={"B7D8DE"}  borderhovercolor={""} bghovercolor={"b0cacf"} onClick={() => teste("Longo")} message={"Longo"}/>
-            <App_Button bgcolor={"FFF3CD"} bordercolor={"D5C799"}  borderhovercolor={""} bghovercolor={"ccc2a3"} onClick={() => teste("Custom")} message={"Customizado"}/>
-                                    
+            {
+                RenderStatus(current_step)     
+            }
+           
         </div>
 
     </div>

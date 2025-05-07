c57c954034a9aa4b8b783130d721927aebf122b4
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-05-07 15:28:19 -0300

    Feature: adiciona reatividade a pagina user settings

--- src/pages/UserScreen.tsx
+++ src/pages/UserScreen.tsx
@@ -1,38 +1,118 @@
-import Header from "../components/Header"
-
+import { useState } from "react"
+import Header from "../components/Header_navigation"
+import App_Button from "../components/App_Button"
 
 
 
 export default function UserScreen() {
+    const [edit_profile, set_edit_profile] = useState(0)
+    const [see_history, set_see_history] = useState(0)
+    const [see_details, set_see_details] = useState(0)
+
+    function edit_controller(status:number)
+    {
+        switch(status)
+        {
+            case 0:
+                {
+                    return (<>
+                        <div style={{width:"100%", height:"20%", display:"flex", justifyContent:"center"}}>
+                            <App_Button bgcolor={""} bordercolor={""} borderhovercolor={""} bghovercolor={""} onClick={() => set_edit_profile(edit_profile + 1)} message={""}/>
+                        </div>
+                    </>)
+                    
+                }
+            case 1:
+                {
+                return <>
+                <div style={{display:'flex', flexDirection:"column", width: "100%", height:"100%"}}>
+                    <div id="HOME-USER-OPTIONS-HEAD" 
+                        style={{width: "100%", height: "10%", backgroundColor: "#D3EAF2", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#b7dbe8"}}>
+                        <h1>Editar Perfil</h1>
+                    </div>
+                    <div className="User-Options" style={{width: "100%", height:"100%"}}>
+                    </div>
+                </div>
+                </>
+                }
+        }
+    }
+
+    function history_controller(status:number)
+    {
+        switch(status)
+        {
+            case 0:
+                {
+                    return (<>
+                        <div style={{width:"100%", height:"20%", display:"flex", justifyContent:"center"}}>
+                            <App_Button bgcolor={""} bordercolor={""} borderhovercolor={""} bghovercolor={""} onClick={() => set_see_history(see_history + 1)} message={""}/>
+                        </div>
+                    </>)
+                    
+                }
+            case 1:
+                {
+                return <>
+
+                     <div id="HOME-USER-OPTIONS-HEAD" 
+                    style={{width: "100%", height: "19%", backgroundColor: "#E3D4F3 ", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#cbb3e6"}}>
+                    <h1>Ver Histórico</h1>
+                    </div>
+                    <div className="User-Options" style={{width: "100%", height:"100%"}}>
+                    </div>
+                </>
+            }
+        }
+    }
+
+    function detail_controller(status:number)
+    {
+        switch(status)
+        {
+            case 0:
+                {
+                    return (<>
+                        <div style={{width:"100%", height:"20%", display:"flex", justifyContent:"center"}}>
+                            <App_Button bgcolor={""} bordercolor={""} borderhovercolor={""} bghovercolor={""} onClick={() => set_see_details(see_details + 1)} message={""}/>
+                        </div>
+                    </>)
+                    
+                }
+            case 1:
+                {
+                return <>
+                    <div id="HOME-USER-OPTIONS-HEAD" 
+                    style={{width: "100%", height: "10%", backgroundColor: "#B4FFFB", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#82D0D5"}}>
+                    <h1>Detalhes do Usuário</h1>
+                    </div>
+                    <div className="User-Options" style={{width: "100%", height:"100%"}}>
+                    </div>
+                </>
+                }
+        }
+    }
+
     return (
     <>
-     <div id="container">
-        <Header Username={""} User_Completed_Quizes={0} User_Completed_Questions={0} User_Completed_Exams={0} User_Favorite_Topic={0}/>
+    <div id="container">
+        <Header Username={""} />
 
+        <div style={{width: "100%", height: "80%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+                <div style={{width:"50%", height: "50%", marginBottom:"2%"}}>
+                    {edit_controller(edit_profile)}
+                </div>
 
-        <div id="HOME-USER-OPTIONS-HEAD" 
-            style={{width: "60%", height: "5%", backgroundColor: "#B4FFFB", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#82D0D5"}}>
-            <h1>Edit profile (name, avatar, theme)</h1>
-        </div>
-        <div className="User-Options">
-        </div>
+                <div style={{width:"50%", height: "50%", marginBottom:"2%"}}>
+                    {history_controller(see_history)}
+                </div>
 
-        <div id="HOME-USER-OPTIONS-HEAD" 
-            style={{width: "60%", height: "5%", backgroundColor: "#B4FFFB", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#82D0D5"}}>
-            <h1>Ver Histórico</h1>
-        </div>
-        <div className="User-Options">
-        </div>
+                <div style={{width:"50%", height: "50%", marginBottom:"2%"}}>
+                    {detail_controller(see_details)}
+                </div>     
 
-        <div id="HOME-USER-OPTIONS-HEAD" 
-            style={{width: "60%", height: "5%", backgroundColor: "#B4FFFB", marginLeft: "auto", marginRight: "auto", borderStyle:"solid", borderWidth:"5px", borderColor:"#82D0D5"}}>
-            <h1>Detalhes do Usuário</h1>
         </div>
-        <div className="User-Options">
-        </div>
-
-
-     </div>
+    </div>
     </>
 
 )

31877926b3de95063e3b9d558bb951cf015261c6
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-06-06 13:09:46 -0300

    Feature: componentiza a funcao de prova com tamanho customizado

--- src/pages/ExamScreen.tsx
+++ src/pages/ExamScreen.tsx
@@ -6,6 +6,7 @@ import Page_Exam_SelectAditionals from "../components/Page_Exam_SelectAditionals
 import { Topics_screen_MainTopicSelection } from "../components/QuestionsSubScreenComponents/MainTopicSelection";
 import { Topics_screen_QuestionSourceSelection } from "../components/QuestionsSubScreenComponents/QuestionSourceSelection";
 import { Topics_screen_SubTopicSelection } from "../components/QuestionsSubScreenComponents/SubTopicSelection";
+import CustomLengthExam from "../components/Page_ExamScreen_CustomLengthExam";
 
 /***
  * 
@@ -142,120 +143,24 @@ export default function ExamScreen() {
         />
       );
 
-     case 3:
-      switch (custom_length_switch) {
-        case 0:
-          return (
-            <>
-              <h1
-                style={{
-                  color: "#666666",
-                  textAlign: "center",
-                  alignContent: "center",
-                }}
-              >
-                Digite o número de perguntas que você deseja!
-              </h1>
-              <input
-                className="Text_Field"
-                style={{ height: "10%", alignSelf: "center" }}
-                type="number"
-                onChange={(e) => {
-                  const val = parseInt(e.target.value);
-                  if (!isNaN(val)) Select_Length(val);
-                }}
-              />
-              <div
-                style={{
-                  width: "100%",
-                  height: "60%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignContent: "center",
-                }}
-              >
-                <App_Button
-                  bgcolor={"D9F2E6"}
-                  bordercolor={"a7d1bc"}
-                  borderhovercolor={"91baa6"}
-                  bghovercolor={"c1d9cd"}
-                  onClick={() => {
-                    set_custom_length_switch(1);
-                  }}
-                  message={"Iniciar"}
-                />
-                <App_Button
-                  bgcolor={"f5abab"}
-                  bordercolor={"db9797"}
-                  borderhovercolor={"c78787"}
-                  bghovercolor={"e39898"}
-                  onClick={() => {
-                    set_current_page_status(0);
-                  }}
-                  message={"Voltar"}
-                />
-              </div>
-            </>
-          );
+    case 3:
+      //TODO FIX THIS FUCKING MESS!!!!!
+      <CustomLengthExam 
+        custom_length_switch={custom_length_switch}
+        set_custom_length_switch={set_custom_length_switch}
+        set_current_page_status={set_current_page_status}
+        set_picked_question_topics={set_picked_question_topics}
+        set_current_topic_state={set_current_topic_state}
+        current_topic_state={current_topic_state}
+        set_picked_question_source={set_picked_question_source}
+        Select_Length={Select_Length} 
+        set_current_component_status={set_current_component_status} 
+        set_current_option={function (option: number): void {
+          throw new Error("Function not implemented.");
+        } }      />
+    }}
 
-        case 1:
-          switch (current_topic_state) {
-            case 0:
-              return (
-                <div style={{ width: "100%", paddingTop: "5%" }}>
-                  <Topics_screen_MainTopicSelection
-                    set_picked_question_topics={set_picked_question_topics}
-                    set_current_page_status={set_custom_length_switch}
-                    set_current_component_status={set_current_topic_state}
-                    set_current_page_status_value={0}
-                    header_bg_color="B4FFFB"
-                    border_color="82D0D5"
-                  />
-                </div>
-              );
-
-            case 1:
-              return (
-                <div style={{ width: "100%", paddingTop: "5%" }}>
-                  <Topics_screen_SubTopicSelection
-                    set_picked_question_topics={() => {}}
-                    set_current_component_status={set_current_topic_state}
-                    header_bg_color="B4FFFB"
-                    border_color="82D0D5"
-                    set_current_page_status_value={0}
-                  />
-                </div>
-              );
-
-            case 2:
-              return (
-                <div style={{ width: "100%", paddingTop: "5%" }}>
-                  <Topics_screen_QuestionSourceSelection
-                    set_picked_question_topics={set_picked_question_source}
-                    set_current_component_status={set_current_topic_state}
-                    header_bg_color="B4FFFB"
-                    border_color="82D0D5"
-                    set_current_option={() => {
-                      throw new Error("Function not implemented.");
-                    }}
-                    set_current_page_status={() => {
-                      throw new Error("Function not implemented.");
-                    }}
-                  />
-                </div>
-              );
-
-            default:
-              console.error("Invalid topic state: ", current_topic_state);
-              return <p>Erro inesperado na seleção de tópicos.</p>;
-          }
-
-        default:
-          console.error("Invalid custom_length_switch: ", custom_length_switch);
-          return <p>Erro inesperado no modo customizado.</p>;
-    }}}
     return (
-    
     <>
     {/** Creating an exam will have 3 steps 
      *  The Length 
@@ -283,6 +188,6 @@ export default function ExamScreen() {
 
     </div>
     </>
+    )
+  }
 
-)
-}
